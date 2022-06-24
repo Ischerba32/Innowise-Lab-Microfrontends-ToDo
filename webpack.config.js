bb233c36fcda6310fe106f3fdd15d82fe59f745c
Igor Scherba <i.scherba32@gmail.com>
2022-06-24 17:10:53 +0300

    resolved scss modules errors

--- webpack.config.js
+++ webpack.config.js
@@ -13,34 +13,27 @@ module.exports = (webpackConfigEnv, argv) => {
     // modify the webpack config however you'd like to by adding to this object
     module: {
       rules: [
-        // {
-        //   test: /\.(scss|css)$/,
-        //   use: ['style-loader', 'css-loader', 'sass-loader'],
-        //   module: true,
-        // },
         {
-          test: /\.scss$/i,
+          test: /\.(scss|css)$/i,
           include: /src/,
           exclude: /node_modules/,
           use: [
             {
               loader: require.resolve('style-loader', {
-                paths: [require.resolve('webpack-config-single-spa')],
+                paths: [require.resolve('webpack-config-single-spa-react-ts')],
               }),
             },
             {
               loader: require.resolve('css-loader', {
-                paths: [require.resolve('webpack-config-single-spa')],
+                paths: [require.resolve('webpack-config-single-spa-react-ts')],
               }),
             },
             {
               loader: require.resolve('sass-loader', {
-                paths: [require.resolve('webpack-config-single-spa')],
+                paths: [require.resolve('webpack-config-single-spa-react-ts')],
               }),
               options: {
-                importLoaders: 1,
                 sourceMap: true,
-                module: true,
               },
             },
           ],
